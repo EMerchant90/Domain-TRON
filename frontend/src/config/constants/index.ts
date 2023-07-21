c1447cc68a362a6e1f4dc7bf8285e050ee120365
adeel <adeel.chaudhary@tron.network>
2023-07-21 18:36:58 +0500

    update tns contract to nile and tron full url

--- frontend/src/config/constants/index.ts
+++ frontend/src/config/constants/index.ts
@@ -1,5 +1,5 @@
 export const FAST_INTERVAL = 10000
 export const SLOW_INTERVAL = 60000
 
-export const TNS_CONTRACT_ADDRESS = '4139d33bf3eba8162eea802fb1a75138ed54e9bd2a'
-export const TRON_FULL_URL = 'http://127.0.0.1:9090'
+export const TNS_CONTRACT_ADDRESS = 'TKhVTdGFwAVLnApmiVadVKz4rTza7RYcXL'
+export const TRON_FULL_URL = 'https://nile.trongrid.io'
