8f7d49ac36c0214a37eb0edf7079f1fc9f109ea6
tronpartnerships <115742536+tronpartnerships@users.noreply.github.com>
2023-09-01 09:45:55 -0700

    Update index.ts

--- frontend/src/config/constants/index.ts
+++ frontend/src/config/constants/index.ts
@@ -1,7 +1,7 @@
 export const FAST_INTERVAL = 10000
 export const SLOW_INTERVAL = 60000
 
-export const TNS_CONTRACT_ADDRESS = 'TN4rjUT3NSQg2vS3LfpXFDJWkm2FQXBgrC'
+export const TNS_CONTRACT_ADDRESS = 'TGLXheveKHGiDoLAQmyW8P4Dqa3n9jWTU8'
 export const TRON_FULL_URL = 'https://api.nileex.io'
 
 export const ENERGY_RENTAL_CONTRACT_ADDRESS ="TX22BBxZz4MtvxHeY32iAefJ3QcJBqbzm1"
