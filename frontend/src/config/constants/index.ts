87765b94a9d729600506076791a7670a2e20d28d
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-09-20 14:57:30 -0400

    Update index.ts

--- frontend/src/config/constants/index.ts
+++ frontend/src/config/constants/index.ts
@@ -2,7 +2,7 @@ export const FAST_INTERVAL = 10000
 export const SLOW_INTERVAL = 60000
 
 export const TNS_CONTRACT_ADDRESS = 'TGW8QMViHCjJsRH2K3BMmrtYhpg6Szq8HT'
-export const TRON_FULL_URL = 'https://api.nileex.io'
+export const TRON_FULL_URL = 'https://api.shasta.trongrid.io'
 
 export const ENERGY_RENTAL_CONTRACT_ADDRESS ="TX22BBxZz4MtvxHeY32iAefJ3QcJBqbzm1"
 
